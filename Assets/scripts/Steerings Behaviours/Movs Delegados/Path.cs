8247fe02a1364645e7d4d5722abecc8c8e33a321
jamesuelo <35700266+jamesuelo@users.noreply.github.com>
2021-03-19 20:30:30 +0100

    Wander tecnicamente funciona y empezado el ath following

---
+++ Assets/scripts/Steerings Behaviours/Movs Delegados/Path.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+
+    [SerializeField] private List<GameObject> nodos;
+
+
+
+
+
+
+
+    public Vector3 GetPosition(float param) {
+
+
+    }
+
+
+    public int GetParam(Vector3 characterPosition, int currentPosition) {
+
+    }
+}
--- Assets/scripts/Steerings Behaviours/Movs Delegados/Wander.cs
+++ Assets/scripts/Steerings Behaviours/Movs Delegados/Wander.cs
@@ -1,11 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/**
+
 public class Wander : Face
 {
+    
+    public float wanderOffset;
+    public float wanderRadius;
+    public float wanderRate;
+    public float wanderOrientation;
+
+    private Vector3 AsVector(float o) {
+        return new Vector3(Mathf.Cos(o), 0, Mathf.Sin(o));
+    }
+
     override public Steering GetSteering(AgentNPC agent)
     {
 
+        wanderOrientation += wanderRate * Random.Range(-1.0f, 1.0f);
+
+        target.Orientation = wanderOrientation+ agent.Orientation;
+        target.transform.position = agent.Position + wanderOffset * AsVector(agent.Orientation);
+        target.transform.position += wanderRadius * AsVector(target.Orientation);
+        Steering steer;
+
+        steer = base.GetSteering(agent);
+
+        steer.linear = agent.maxAcceleration * AsVector(agent.Orientation);
+        return steer;
     }
-}**/
+}
