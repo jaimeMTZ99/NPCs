0ac25f9d510d1ff4af86ffcdf59510e92640773f
jamesuelo <35700266+jamesuelo@users.noreply.github.com>
2021-04-26 15:10:08 +0200

    capitulo 1: Desesperacion

--- Assets/scripts/Steerings Behaviours/Formations/Fijos/Circle.cs
+++ Assets/scripts/Steerings Behaviours/Formations/Fijos/Circle.cs
@@ -14,13 +14,13 @@ public class Circle : MonoBehaviour
     private List<AgentNPC> agentes = new List<AgentNPC>();
 
     private List<AgentNPC> asignaciones;
-    private bool vuelta;
     
     private Vector3 pri = new Vector3(0,0,1);
     private Vector3 seg = new Vector3(1,0,0);
     private Vector3 ter = new Vector3(0,0,-1);
     private Vector3 cua = new Vector3(-1,0,0);
 
+    private int[] oriGrid;
     private float ori1 =0;
     private float ori2 =-Mathf.PI/2;
     private float ori3 =Mathf.PI;
@@ -31,7 +31,7 @@ public class Circle : MonoBehaviour
 
     void Start() {
         asignaciones = new List<AgentNPC>();
-
+        oriGrid = new int[4];
         //metemos los agentes que podemos para la formacion
         foreach (AgentNPC a in agentes) {
             if (asignaciones.Count<ranuras){
@@ -44,7 +44,7 @@ public class Circle : MonoBehaviour
             }
         }
         UpdateSlots();
-        vuelta = true;
+
     }
     void Update(){
         foreach (AgentNPC a in asignaciones)
@@ -65,17 +65,14 @@ public class Circle : MonoBehaviour
         lider.orientation *= -1; 
         for (int i = 0; i < asignaciones.Count; i++) {
             Vector3 pos = GetPosition(i);
-            float ori = GetOrientation(i);
-
-            var result = new Vector3(Mathf.Cos(lider.orientation) * pos.x -  Mathf.Sin(lider.orientation) * pos.z,
-                0,
-                Mathf.Sin(lider.orientation) * pos.x + Mathf.Cos(lider.orientation) * pos.z);
+            int oriReal = oriGrid[i];
+            float ori = GetOrientation(oriReal);
 
             GameObject a = GameObject.Find("FC " + (i+1));
             Agent invisible = a.GetComponent<Agent>();
 
 
-            invisible.transform.position =result;
+            invisible.transform.position =pos;
             invisible.orientation =-(lider.orientation + ori);
 
             asignaciones[i].GetComponent<SeekAcceleration>().target = invisible;
@@ -99,16 +96,19 @@ public class Circle : MonoBehaviour
             resultado = Mathf.PI/2;
         }
         /**
-            if (asignaciones[numero].transform.position == (pri*radio+centro)){
+            if ( new Vector3 (Mathf.Round(asignaciones[numero].transform.position.x/radio - centro.x), 0 ,Mathf.Round(asignaciones[numero].transform.position.z/radio - centro.z)) == pri){
                 resultado = ori1;
-            } else if(asignaciones[numero].transform.position == (seg*radio+centro)){
+
+            } else if(new Vector3 (Mathf.Round(asignaciones[numero].transform.position.x/radio - centro.x), 0 ,Mathf.Round(asignaciones[numero].transform.position.z/radio - centro.z)) == seg){
                 resultado = ori2;
-            } else if(asignaciones[numero].transform.position == (ter*radio+centro)){
+
+            } else if(new Vector3 (Mathf.Round(asignaciones[numero].transform.position.x/radio - centro.x), 0 ,Mathf.Round(asignaciones[numero].transform.position.z/radio - centro.z)) == ter){
                 resultado = ori3;
+
             } else {
                 resultado = ori4;
             }
-                **/
+              **/  
         return resultado;
     }
 
@@ -117,11 +117,11 @@ public class Circle : MonoBehaviour
         Vector3 aux2 = seg;
         Vector3 aux3 = ter;
         Vector3 aux4 = cua;
-
         pri = aux2;
         seg= aux3;
         ter = aux4;
         cua = aux1;
+
     }
 
     public void GirarDer(){
@@ -133,14 +133,48 @@ public class Circle : MonoBehaviour
         ter= aux2;
         cua = aux3;
         pri = aux4;
+
     }
 
     public void GirarMatriz(){
+        Vector3 pri1 = new Vector3(0,0,1);
+        Vector3 seg1 = new Vector3(1,0,0);
+        Vector3 ter1 = new Vector3(0,0,-1);
+        Vector3 cua1 = new Vector3(-1,0,0);
+
+
+
         if (centro.magnitude - asignaciones[0].transform.position.magnitude >0)
             GirarDer();
         else
             GirarIzq();
         
+
+
+        Debug.Log(pri);
+        if(pri == pri1 ){
+            for(int i = 0; i< 4; i++)
+            {
+                oriGrid[i] = i;
+            }
+        }
+        else if (pri == seg1){
+            oriGrid[0] = 1;
+            oriGrid[1] = 2;
+            oriGrid[2] = 3;
+            oriGrid[3] = 0;
+        }
+        else if (pri == ter1){
+            oriGrid[0] = 2;
+            oriGrid[1] = 3;
+            oriGrid[2] = 0;
+            oriGrid[3] = 1;
+        } else{
+            oriGrid[0] = 3;
+            oriGrid[1] = 0;
+            oriGrid[2] = 1;
+            oriGrid[3] = 2;
+        }
     }
     // calcula la posicion
     public Vector3 GetPosition(int numero) {
