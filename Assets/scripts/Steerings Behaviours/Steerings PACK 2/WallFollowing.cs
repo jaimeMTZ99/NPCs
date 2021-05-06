3d37b73706027f96c709aa1d75d9c9f94636d83d
jajavixu <jlof000@gmail.com>
2021-05-06 17:09:02 +0200

    uwuall following

--- Assets/scripts/Steerings Behaviours/Steerings PACK 2/WallFollowing.cs
+++ Assets/scripts/Steerings Behaviours/Steerings PACK 2/WallFollowing.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class WallFollowing : SeekAcceleration
 {
     public GameObject goWF;
-    public float predictTime = 0.1f;
+    public int predictTime = 1;
     private Vector3 futurePos;
     public float distancia = 3;
     [SerializeField]
@@ -15,8 +15,7 @@ public class WallFollowing : SeekAcceleration
     void Start(){
         goWF = new GameObject("WallFollowing");
         target = goWF.AddComponent<Agent>() as Agent;
-        target.extRadius = 1.5f;
-        target.intRadius = 1.5f;
+
         col = new List<Collider>();
         foreach (GameObject b in walls)
         {
@@ -29,15 +28,23 @@ public class WallFollowing : SeekAcceleration
     public override Steering GetSteering(AgentNPC agent){
 
         futurePos = agent.transform.position+agent.Velocity*predictTime;
+
         GameObject pared = null;
         float puntoMasCercano = 99999;
+        Vector3 closestPoint = Vector3.zero;
+        Vector3 closestPointActualWall = Vector3.zero;
+        Vector3 distance = Vector3.zero;
         for (int i = 0; i< col.Count; i++){
-            Vector3 closestPoint = col[i].ClosestPoint(futurePos);
-            if(closestPoint.magnitude < puntoMasCercano){
-                puntoMasCercano = closestPoint.magnitude;
+            closestPoint = col[i].ClosestPoint(futurePos);
+            distance = futurePos - closestPoint;
+            if(distance.magnitude < puntoMasCercano){
+                puntoMasCercano = distance.magnitude;
+                closestPointActualWall = closestPoint;
                 pared = walls[i];
+                
             }
         }
+
         Vector3 normale = Vector3.zero;
         Vector3 dir = pared.transform.position - futurePos;
         RaycastHit hit;
@@ -47,7 +54,8 @@ public class WallFollowing : SeekAcceleration
             normale = hit.normal;
         }
 
-        normale = normale + normale.normalized*distancia;
+        normale = closestPointActualWall + normale*distancia;
+        normale.y = 0;
         target.transform.position = normale;
         return base.GetSteering(agent);
     }
