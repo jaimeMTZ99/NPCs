12b884c1ec027b9734e045292231e2d1883c6c63
jamesuelo <35700266+jamesuelo@users.noreply.github.com>
2021-05-08 20:57:36 +0200

    Escenario y personajes montados

---
+++ Assets/scripts/Estrategia/NPC.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public enum TipoUnidad{
+        Brawler,
+        Ranged,
+        Medic
+    }
+
+    public enum Equipo{
+        Spain,
+        France
+    }
+
+    public TipoUnidad tipo;
+    public Equipo team;
+
+    public int health;
+
+    public int meleeDamage;
+    public int meleeDamageCrit;
+    public float rangeMelee;
+    public float meleeAttackSpeed;
+
+    public int rangedDamage;
+    public int rangedDamageCrit;
+    public float rangeRanged;
+    public float rangedAttackSpeed;
+
+    public float speed;
+
+    public int captureRatio;
+}
