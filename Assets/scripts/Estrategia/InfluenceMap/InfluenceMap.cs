8857425ef27f0fbd90bd207e9c1e483b8b01b48e
jamesuelo <35700266+jamesuelo@users.noreply.github.com>
2021-05-12 20:37:48 +0200

    primera version mapa influencias

---
+++ Assets/scripts/Estrategia/InfluenceMap/InfluenceMap.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfluenceMap : Graph
+{
+    public List<NPC> unidades;
+    // works as vertices in regular graph
+    GameObject[] posiciones;
+
+    void Awake()
+    {
+        if (unidades == null)
+            unidades = new List<NPC>();
+    }
+
+    public void AddUnit(NPC u)
+    {
+        if (unidades.Contains(u))
+            return;
+        unidades.Add(u);
+    }
+
+    public void RemoveUnit(NPC u)
+    {
+        unidades.Remove(u);
+    }
+    public void ComputeInfluenceSimple()
+    {
+        int vId;
+        GameObject vObj;
+        Punto v;
+        float dropOff;
+        List<Vertex> pendientes = new List<Vertex>();
+        List<Vertex> visitados = new List<Vertex>();
+        List<Vertex> frontera;
+        Vertex[] vecinos;
+        foreach(NPC u in unidades)
+        {
+            Vector3 uPos = u.transform.position;
+            Vertex vert = GetNearestVertex(uPos);
+            pendientes.Add(vert);
+            // BFS for assigning influence
+            for (int i = 1; i <= u.radius; i++)
+            {
+                frontera = new List<Vertex>();
+                foreach (Vertex p in pendientes)
+                {
+                    if (visitados.Contains(p))
+                        continue;
+                    visitados.Add(p);
+                    v = p as Punto;
+                    dropOff = u.GetDropOff(i);
+                    v.SetValue(u.team, dropOff);
+
+                    vecinos = GetNeighbours(vert);
+                    frontera.AddRange(vecinos);
+                }
+            pendientes = new List<Vertex>(frontera);
+            }
+        
+        }
+        
+    }
+}
---
+++ Assets/scripts/Estrategia/InfluenceMap/Punto.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punto : Vertex
+{
+    public Equipo equipo;
+    public float valor = 0f;
+
+
+    public bool SetValue(Equipo f, float v)
+    {
+        bool isUpdated = false;
+        if (v > valor)
+            {
+            valor = v;
+            equipo = f;
+            isUpdated = true;
+            }
+        return isUpdated;
+    }
+}
